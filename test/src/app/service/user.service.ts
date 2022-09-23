f54eb0fbbf1a1c0e80f76fdbef46c612da9f5b44
Jessica JJ Bryant <jessicajjbryant@Jessicas-iMac.local>
2022-09-23 11:42:40 -0400

    Add HttpClient import and constructor to user.service.ts. Add functions getUsers and getUser to user.service.ts. Use Angular CLI to generate interface in test folder

--- test/src/app/service/user.service.ts
+++ test/src/app/service/user.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient ) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
+  }
+  getUsers(): Observable<User> {
+    return this.http.get<User>('https://jsonplaceholder.typicode.com/users/1');
+  }
 }
